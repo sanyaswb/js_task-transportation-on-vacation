3bc0ae8d3a38aeb59f00d66f5d70b4c18ef6958b
Oleksandr Merkulov <odin@terradeus.org>
2025-12-27 15:03:14 +0200

    1 task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const smallDiscont = 20;
+  const bigDiscont = 50;
+  const dailyPrice = 40;
+  const totalRentalPrice = days * dailyPrice;
+
+  if (days >= 3 && days < 7) {
+    return totalRentalPrice - smallDiscont;
+  }
+
+  if (days >= 7) {
+    return totalRentalPrice - bigDiscont;
+  }
+
+  return totalRentalPrice;
 }
 
 module.exports = calculateRentalCost;
