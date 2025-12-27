cd0bbdfe98d945d533369223a92884dfb5080f54
Oleksandr Merkulov <odin@terradeus.org>
2025-12-27 15:28:29 +0200

    rename variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,19 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const longTerm = 7;
-  const shortTerm = 3;
-  const smallDiscount = 20;
-  const bigDiscount = 50;
-  const dailyPrice = 40;
-  const totalRentalPrice = days * dailyPrice;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+  const SAMALL_DISCOUNT = 20;
+  const BIG_DISCOUNT = 50;
+  const DAILY_PRICE = 40;
 
-  if (days >= shortTerm && days < longTerm) {
-    return totalRentalPrice - smallDiscount;
+  const totalRentalPrice = days * DAILY_PRICE;
+
+  if (days >= SHORT_TERM && days < LONG_TERM) {
+    return totalRentalPrice - SAMALL_DISCOUNT;
   }
 
-  if (days >= longTerm) {
-    return totalRentalPrice - bigDiscount;
+  if (days >= LONG_TERM) {
+    return totalRentalPrice - BIG_DISCOUNT;
   }
 
   return totalRentalPrice;
